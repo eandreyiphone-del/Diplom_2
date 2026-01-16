f43df83e3e558a8f176002b998c32d6950fca3c8
Andrey Egorov <egorov.andrei3682@yandex.ru>
2026-01-16 09:33:11 +0400

    First commit

---
+++ src/main/java/ru/yandex/practicum/OrderSteps.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum;
+
+import io.qameta.allure.Step;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderSteps {
+
+    public static final String ORDERS = "/api/orders";
+
+    @Step("Создание заказа")
+    public ValidatableResponse createOrder(User user, Order order) {
+        return given()
+                .auth().oauth2(user.getAccessToken())
+                .body(order)
+                .when()
+                .post(ORDERS)
+                .then();
+    }
+
+    @Step("Создание заказа без авторизации")
+    public ValidatableResponse createOrderWithoutAuthorization(Order order) {
+        return given()
+                .body(order)
+                .when()
+                .post(ORDERS)
+                .then();
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/RestConfig.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum;
+
+public class RestConfig {
+    public static final String HOST = "https://stellarburgers.education-services.ru";
+}
